1c8677ed2a2a0a458078645e9b9a5442083687e5
Marius <realslimwedy@icloud.com>
2023-07-16 12:38:34 +0200

    yolo wifi connection initialization working

--- code/main.py
+++ code/main.py
@@ -4,6 +4,10 @@ from djitellopy import tello
 import tello_package as tp
 import time
 
+# Connect to TELLO drone wifi
+tp.connect_to_wifi("TELLO-9C7357")
+#time.sleep(7)
+
 # Initialize drone & connect
 tp.init_keyboard_control()
 me = tello.Tello()
@@ -24,7 +28,6 @@ me.land()
 '''
 
 # control drone via keyboard
-
 while True:
     vals=tp.keyboard_control_drone(me, (0,0,0,0))
     me.send_rc_control(vals[0],vals[1],vals[2],vals[3])
--- code/tello_package/__init__.py
+++ code/tello_package/__init__.py
@@ -1,1 +1,2 @@
 from .keyboard_control import init_keyboard_control, get_key, keyboard_control_drone, save_image
+from .connect_wifi import connect_to_wifi
